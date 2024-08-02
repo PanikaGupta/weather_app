a7a0f54edb87dd675fc3f4de38db90fd333f0df4
Panika Gupta <107405215+PanikaGupta@users.noreply.github.com>
2024-08-02 23:20:52 +0530

    weather_app

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const input=document.getElementById("city-input");
+const button=document.getElementById("search-button");
+const city_name=document.getElementById("city-name");
+const city_time=document.getElementById("city-time");
+const city_temp=document.getElementById("city-temp");
+
+
+// http://api.weatherapi.com/v1/current.json?key=f0f6b839817348d691e101527242607&q=${input}&aqi=yes
+
+async function getdata(city_name){
+    const result =await fetch(`http://api.weatherapi.com/v1/current.json?key=f0f6b839817348d691e101527242607&q=${city_name}&aqi=yes`);
+    return await result.json();
+}
+button.addEventListener("click",async ()=>{
+    const cityname=input.value;
+    const res=await getdata(cityname);
+    console.log(res);
+    city_name.innerText=`${res.location.name} ,${res.location.region} ,${res.location.country}`
+    city_time.innerText=`${res.location.localtime}`
+    city_temp.innerText=`${res.current.temp_c} degree celsius`
+})
+
